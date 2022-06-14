523fdcfb5bbd387c275ba8d823f43642f19123f2
DoGukKim <dgkim3811@naver.com>
2022-06-14 13:32:56 +0900

    bfs

---
+++ graph/BFS.js
@@ -0,0 +1,23 @@
+const BFS = (graph, source) => {
+  const queue = [source];
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+    console.log(current);
+    for (const neighbor of graph[current]) {
+      queue.push(neighbor);
+    }
+  }
+};
+
+const graph = {
+  a: ["b", "c"],
+  b: ["d"],
+  c: ["e"],
+  d: ["f"],
+  e: [],
+  f: [],
+};
+
+const result = BFS(graph, "a");
+console.log(result);
