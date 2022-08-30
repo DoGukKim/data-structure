6517a33aec6e6c43d12892a8f6a8e77ef4dbe9dd
DoGukKim <dgkim3811@naver.com>
2022-08-30 12:51:57 +0900

    해시, 스택 큐, 연결 리스트 복습

--- hash/hash.js
+++ hash/hash.js
@@ -1,41 +1,44 @@
 const defaultHashTableSize = 30;
+
 class HashTable {
   constructor() {
-    this.buckets = Array(defaultHashTableSize).fill(null);
-    this.keys = {};
+    this.buckets = Array.from({ length: defaultHashTableSize }, () => null);
   }
 
   hash(key) {
+    // 각 문자의 UTF 코드로 합산하여 키 값을 생성한다.
     const hash = Array.from(key).reduce(
-      (acc, keySymbol) => acc + keySymbol.charCodeAt(0),
+      (acc, cur) => acc + cur.charCodeAt(),
       0
     );
+    // 해시 테이블의 사이즈로 제한하기 위해서 나머지 연산 활용.
     return hash % this.buckets.length;
   }
 
   set(key, value) {
     const keyHash = this.hash(key);
-    this.keys[key] = keyHash;
     if (this.buckets[keyHash] !== null) {
+      // 충돌이 발생하면 분리 연결법으로 해결한다.
       this.buckets[keyHash] = [...this.buckets[keyHash], [key, value]];
-    } else {
-      this.buckets[keyHash] = [[key, value]];
+      return;
     }
+    this.buckets[keyHash] = [[key, value]];
   }
 
   get(key) {
-    return this.buckets[this.hash(key)].find((i) => i[0] === key)[1] ?? null;
+    // 분리연결법으로 배열에 저장되었을 때 값을 받아오기 위해 find로 처리.
+    return (
+      this.buckets[this.hash(key)].find((item) => item[0] === key)[1] ?? null
+    );
   }
 
   delete(key) {
     const keyHash = this.hash(key);
-    const bucket = this.buckets[keyHash];
-    if (bucket !== null) {
-      const isHas = bucket.find((i) => i[0] === key);
+    if (this.buckets[keyHash] !== null) {
+      const isHas = this.buckets[keyHash].find((item) => item[0] === key);
       if (isHas) {
-        delete this.keys[key];
         this.buckets[keyHash] = this.buckets[keyHash].filter(
-          (i) => i[0] !== key
+          (item) => item[0] !== key
         );
       }
     }
